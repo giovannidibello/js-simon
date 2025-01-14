df8807ab3cbe386a5ad3d09b10de8a1aabd94ec3
Giovanni <dibellogio@gmail.com>
2025-01-14 15:06:26 +0100

    add function random numbers

--- js/script.js
+++ js/script.js
@@ -6,23 +6,73 @@ const countDown = document.getElementById("countdown");
 const instructions = document.getElementById("instructions");
 const numbersList = document.getElementById("numbers-list");
 const answersForm = document.getElementById("answers-form");
-const numbersForm = document.querySelectorAll("form-control");
+const numbersForm = document.querySelectorAll(".form-control");
 const button = document.querySelector("button");
 const message = document.getElementById("message");
 
-// creo una funzione che genera numeri random pescati da un array
+// invoco la funzione dei numeri random
+estraiNumero();
 
-    // faccio visualizzare i numeri generati in pagina
+
+
+
+
+
 
 // creo una funzione asincrona che imposta un timer di 30 secondi
 
-    // i numeri scompaiono dalla pagina
+// i numeri scompaiono dalla pagina
 
-    // allo scadere faccio visualizzare in pagina 5 campi di input
+// allo scadere faccio visualizzare in pagina 5 campi di input
 
 // creo una funzione al click del pulsante
 
-    // la funzione confronta i numeri inseriti con quelli visualizzati all'inizio
+// la funzione confronta i numeri inseriti con quelli visualizzati all'inizio
 
-    // restituisce se e quanti numeri sono stati individuati
+// restituisce se e quanti numeri sono stati individuati
+
+
+// FUNCTION
+
+// creo una funzione che genera numeri random pescati da un array
+
+// funzione estrai numero
+function estraiNumero() {
+
+    // creo un array con tutti i numeri da generare
+    const numbersToExtract = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+        41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+    ];
+
+    // inizializzo l'array dei numeri estratti
+    const numbersExtract = [];
+
+    for (let i = 0; i < 5; i++) {
+
+        // estraggo un numero con indice casuale
+        const indiceCasuale = Math.floor(Math.random() * numbersToExtract.length);
+        // elimino dall'array numeri da estrarre il numero estratto
+        const number = numbersToExtract.splice(indiceCasuale, 1)[0];
+        // metto il numero estratto in un altro array
+        numbersExtract.push(number);
+
+        console.log(number);
+
+        // // Pulisco la lista esistente
+        numbersList.innerHTML = "";
+
+        // // creo un <li> per ogni numero estratto e lo aggiungo alla lista
+        numbersExtract.forEach((number) => {
+            const li = document.createElement("li");
+            li.textContent = number;
+            numbersList.appendChild(li);
+        });       
+
+    }
+    
+}
 
